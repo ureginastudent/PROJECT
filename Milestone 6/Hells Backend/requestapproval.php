25867bae020ac2742e3f7424c369db4762b78ee2
S <S>
2018-03-26 17:05:28 -0600

    Added sessions and approval

---
+++ Milestone 6/Hells Backend/requestapproval.php
@@ -0,0 +1,28 @@
+<?php
+	include('connect.php');
+	include('session.php');
+	
+	if ($user_type != "analyst")
+	{
+		mysqli_close($conn);
+		die("Invalid user type");
+	}
+	
+	$software_id = isset($_GET['software_id']) ? $_GET['software_id'] : '';
+	$user_id_software = isset($_GET['user_id']) ? $_GET['user_id'] : '';
+	
+	if (empty($user_id) || empty($software_id))
+	{
+		mysqli_close($conn);
+		die("Invalid parameters");
+	}
+	
+	$safe_user_id = mysqli_real_escape_string($conn, $user_id_software);
+	$safe_software_id = mysqli_real_escape_string($conn, $software_id);
+	
+	$sql = "UPDATE software_request SET approved_status='pending approval' WHERE software_id = '$safe_software_id' and user_id = '$safe_user_id'";
+	$result = mysqli_query($conn, $sql);
+
+	echo $result;
+	mysqli_close($conn);
+?>
---
+++ Milestone 6/Hells Backend/session.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+
+	$user_id = isset($_SESSION['login_id']) ? $_SESSION['login_id'] : '';
+	$user_type = isset($_SESSION['login_type']) ? $_SESSION['login_type'] : '';
+
+	if (empty($user_id) || empty($user_type))
+	{
+		session_destroy();
+		die();
+		mysqli_close($conn);
+	}
+	
+	$sql = "SELECT * FROM " . $user_type . " WHERE " . $user_type . "_id = '$user_id'";
+	
+	
+	
+	if (mysqli_query($conn, $sql)) 
+	{
+		$result        = mysqli_query($conn, $sql);
+		$row           = mysqli_fetch_assoc($result);
+		
+		$login_session = $row[$user_type . "_id"];
+    
+		if (!isset($login_session)) {
+			session_destroy();
+			mysqli_close($conn);
+			die();
+		}
+	}
+?>
